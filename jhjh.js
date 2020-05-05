dc2345a89f37dc566c145160036c229cf4b67d97
EC2 Default User <ec2-user@ip-172-31-37-22.ap-northeast-2.compute.internal>
2020-05-05 08:54:46 +0000

    fix wrong password allowing entrance for jhjh indexings

--- jhjh.js
+++ jhjh.js
@@ -2,11 +2,13 @@ var password;
 
 var pass1="0905";
 
-password=prompt("지혜의 생일은?",' ');
-
-if (password==pass1){
-  alert('딩동댕!');
-}
-else {
-  alert("Incorrect password-Try again");
+while (password != pass1) {
+  password=prompt("지혜의 생일은?",' ');
+  
+  if (password==pass1){
+    alert('딩동댕!');
+  }
+  else {
+    alert("Incorrect password-Try again");
+  }
 }
