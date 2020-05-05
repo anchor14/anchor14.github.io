3edbf9dac10258148ddaf68049eafe5dcc9cfc40
EC2 Default User <ec2-user@ip-172-31-37-22.ap-northeast-2.compute.internal>
2020-05-05 08:56:27 +0000

    fix second video to come to center

--- miniconcert.js
+++ miniconcert.js
@@ -4,12 +4,12 @@ var pass1="culo";
 
 while (password !== pass1) {
   
-password=prompt('Please enter your password to view this page!',' ');
-
-if (password==pass1){
-  alert('Password Correct! Click OK to enter!');
-}
-else {
-  alert("Incorrect password-Try again");
+  password=prompt('Please enter your password to view this page!',' ');
+  
+  if (password==pass1){
+    alert('Password Correct! Click OK to enter!');
   }
+  else {
+    alert("Incorrect password-Try again");
+    }
 }
