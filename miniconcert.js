4b01515c361d0f3b87c4dd2e5f43aee6d04c8e02
Jay Anchor <jaehurn.nam@gmail.com>
2020-05-19 13:30:01 +0900

    add last day of army

--- miniconcert.js
+++ miniconcert.js
@@ -4,22 +4,20 @@ var pass1="culo";
 var counter = 0;
 
 while (password !== pass1) {
-  
+
   password=prompt('Please enter your password to view this page!',' ');
-  
+
   if (password==pass1){
     alert('Password Correct! Click OK to enter!');
   }
   else {
     alert("Incorrect password-Try again");
     counter++;
-    
+
     if (counter==5) {
       alert("fuck off now");
-      return false;
     }
     }
-    
-  
-}
 
+
+}
