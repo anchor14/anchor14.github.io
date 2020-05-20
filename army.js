265b9a9b676dfb12f63efe940013a65b4e368445
Jay Anchor <jaehurn.nam@gmail.com>
2020-05-20 18:32:01 +0900

    put on password to last day of army page

---
+++ army.js
@@ -0,0 +1,14 @@
+var password;
+
+var pass1="fuck2oc";
+
+while (password != pass1) {
+  password=prompt("충성!",' ');
+
+  if (password==pass1){
+    alert('딩동댕!');
+  }
+  else {
+    alert("Incorrect password-Try again");
+  }
+}
